f00eb00d25cc8e951ff6aaa9d693d9a136363ba0
Martin Fredriksson <martin.fredriksson@vikinganalytics.se>
2020-09-16 12:45:30 +0200

    Source code added.

---
+++ src/tests/my-triples.spec.ts
@@ -0,0 +1,10 @@
+import { Turtles, Graph } from "../rdf-graph"
+
+test('should have size after initialization with turtles', () => {
+    let myTurtles: Turtles = [
+        ['this', 'a', 'turtle'],
+        ['statement']
+    ];
+    const myGraphDB = new Graph(myTurtles); 
+    expect(myGraphDB.size).toBeGreaterThan(0);
+})
