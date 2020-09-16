a3f6eda708b3bc2b54febfecd3ac69f8bae12476
Martin Fredriksson <martin.fredriksson@vikinganalytics.se>
2020-09-16 14:31:22 +0200

    Excluding outfrom git

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isInternalLink = (link) => /^\/(?!\/)/.test(link);
-console.log('Hello, world!');
-//# sourceMappingURL=index.js.map
